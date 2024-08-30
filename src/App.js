4aa505fd0e058a8d9b6dac4b5ccdc79c819fa0cb
Matt Torres <matt.torres@vrinreview.com>
2024-08-30 08:11:11 -0500

    Fixed dark type color

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
     Normal: '#A8A878', Fire: '#F08030', Water: '#6890F0', Electric: '#F8D030', Grass: '#78C850',
     Ice: '#98D8D8', Fighting: '#C03028', Poison: '#A040A0', Ground: '#E0C068', Flying: '#A890F0',
     Psychic: '#F85888', Bug: '#A8B820', Rock: '#B8A038', Ghost: '#705898', Dragon: '#7038F8',
-    Dark: '#705848', Steel: '#B8B8D0', Fairy: '#EE99AC'
+    Dark: '#111111', Steel: '#B8B8D0', Fairy: '#EE99AC'
   };
 
   const correctEffectiveness = {
